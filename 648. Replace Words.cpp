4d39ba449fd66086a4d8529974fc100072063bba
ignacio <nachomonllorc@gmail.com>
2020-01-14 19:34:39 -0300

    agrego

---
+++ 648. Replace Words.cpp
@@ -0,0 +1,80 @@
+﻿https://leetcode.com/problems/replace-words/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <functional> 
+#include <cctype>
+#include <locale>
+#include <string>
+
+
+using namespace std;
+
+std::string& ltrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+    str.erase(0, str.find_first_not_of(chars));
+    return str;
+}
+ 
+std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+    str.erase(str.find_last_not_of(chars) + 1);
+    return str;
+}
+ 
+std::string& trim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+    return ltrim(rtrim(str, chars), chars);
+}
+
+string replaceWords(vector<string>& dict, string sentence) {
+   
+    set<string> hash; 
+    for(int i =0; i<dict.size(); i++) {
+    	hash.insert(dict[i]);
+	}
+    
+    string ans = "";
+    string concat = "";
+    for(int i =0; i<sentence.length(); i++)
+    {
+        if (sentence[i] != ' ')
+        {
+            concat += sentence[i];
+
+            if(hash.find(concat) != hash.end())
+            {
+                ans += concat + " ";
+                concat = "";
+                while (i < sentence.length() && sentence[i] != ' ') i++;
+            }
+        }
+        else
+        {
+            ans += concat + " ";
+            concat = "";
+        }
+    }
+
+    ans += concat + " ";
+    concat = "";
+    
+   	
+   	return trim(ans);
+        
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+  
+  
+    
+
+    
+    return 0;
+}
---
+++ 941. Valid Mountain Array.cpp
@@ -0,0 +1,46 @@
+﻿https://leetcode.com/problems/valid-mountain-array/
+1.	#include <iostream>
+2.	#include <stdio.h>
+3.	#include <vector>
+4.	 
+5.	using namespace std;
+6.	 
+7.	bool validMountainArray(vector<int>& A) {
+8.	   
+9.	   if (A.size() < 3) return false;
+10.	   int max = INT_MIN;
+11.	   int ind_max = -1;
+12.	   for(int i =0; i <A.size(); i++) {
+13.	        if(A[i] > max) {
+14.	            max = A[i];
+15.	            ind_max = i;
+16.	        }
+17.	   }
+18.	   
+19.	   if(ind_max == 0 ||ind_max == A.size() - 1) return false;
+20.	   
+21.	   for(int i =0; i < ind_max; i++) {
+22.	        if(A[i] >= A[i+1]) return false;
+23.	   }
+24.	   
+25.	   for(int i =ind_max; i<A.size(); i++) {
+26.	        if(A[i] <= A[i+1]) return false;
+27.	   }
+28.	   return true;
+29.	}
+30.	 
+31.	int main() {
+32.	   
+33.	    int arr[] =  {0,3,2,1};
+34.	    int size = sizeof(arr)/sizeof(int);
+35.	   
+36.	    std::vector<int> v;
+37.	    for(int i =0; i<size; i++) {
+38.	        v.push_back(arr[i]);
+39.	    }
+40.	   
+41.	    cout << validMountainArray(v) << endl;
+42.	   
+43.	   
+44.	    return 0;
+45.	}
