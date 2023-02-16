1a2fee245688ef67e9bff2598198227ca4311517
nachomonllor <nachomonllorc@gmail.com>
2023-02-16 15:37:34 -0300

    SmallestRangeI

---
+++ 908SmallestRangeI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _908SmallestRangeI
+{
+    //https://leetcode.com/problems/smallest-range-i/description/
+    class Program
+    {
+        public int SmallestRangeI(int[] nums, int k)
+        {
+
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
