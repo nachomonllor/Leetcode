925c3ebd8bf8f5fe05dac906c573a499127dd4d2
nachomonllor <nachomonllorc@gmail.com>
2023-02-16 23:24:18 -0300

    SmallestRangeI inicio

--- 908SmallestRangeI/Program.cs
+++ 908SmallestRangeI/Program.cs
@@ -11,8 +11,15 @@ namespace _908SmallestRangeI
     {
         public int SmallestRangeI(int[] nums, int k)
         {
+            List<int> techo = new List<int>();
+            List<int> piso = new List<int>();
 
-
+            for(int i = 0; i < nums.Length; i++)
+            {
+                techo.Add(nums[i] + k);
+                piso.Add(nums[i] - k);
+            }
+            return 0;
         }
         static void Main(string[] args)
         {
