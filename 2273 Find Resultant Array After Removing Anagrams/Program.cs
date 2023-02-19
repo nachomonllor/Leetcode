372515bad7e4a440442b3637d4aef3949650c7ca
nachomonllor <nachomonllorc@gmail.com>
2023-02-19 01:03:56 -0300

    2273_Find_Resultant_Array_After_Removing_Anagrams Incompleto

---
+++ 2273 Find Resultant Array After Removing Anagrams/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2273_Find_Resultant_Array_After_Removing_Anagrams
+{
+    class Program
+    {
+        public static IList<string> RemoveAnagrams(string[] words)
+        {
+
+            HashSet<string> hash = new HashSet<string>();
+
+            IList<string> ans = new List<string>();
+
+            foreach (string word in words)
+            {
+                string sorted = new string(word.OrderBy(c => c).ToArray());
+
+                if (!hash.Contains(sorted))
+                {
+                    ans.Add(word);
+                }
+                hash.Add(sorted);
+
+            }
+
+            return ans;
+
+        }
+        static void Main(string[] args)
+        {
+
+            string[] words = { "abba", "baba", "bbaa", "cd", "cd" };
+            //Output: ["abba","cd"]
+
+            foreach (string item in RemoveAnagrams(words))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
