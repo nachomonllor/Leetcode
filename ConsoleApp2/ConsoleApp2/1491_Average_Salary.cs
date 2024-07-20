1232310536328bc84552c4f11386ad812b6bcdd1
Ignacio Monllor <nachomonllorc@gmail.com>
2024-07-20 00:55:37 -0300

    1491 Average Salary

---
+++ ConsoleApp2/ConsoleApp2/1491_Average_Salary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class _1491_Average_Salary
+    {
+
+        public double Average(int[] salary)
+        {
+            int min = salary.Min();
+            int max = salary.Max();
+
+            double sum = 0;
+            double cont = 0;
+
+            for(int i = 0; i < salary.Length; i++)
+            {
+                if (salary[i] != min && salary[i] != max)
+                {
+                    sum += salary[i];
+                    cont++;
+                }
+            }
+
+            return sum / cont ;
+        }
+
+
+
+    }
+}
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -39,6 +39,8 @@ namespace ConsoleApp2
 
         }
 
+
+
         static void Main(string[] args)
         {
 
