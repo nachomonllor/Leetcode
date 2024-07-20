043ffabe71db2e0a10789bbad167abbea6c24b90
Ignacio Monllor <nachomonllorc@gmail.com>
2024-07-20 01:12:17 -0300

    _2164_OrdenarPorIndices

---
+++ ConsoleApp2/ConsoleApp2/2164_OrdenarPorIndices.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class _2164_OrdenarPorIndices
+    {
+
+        public int[] SortEvenOdd(int[] nums)
+        {
+            List<int> indicePares = new List<int>();
+            List<int> indicesImpares = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    indicePares.Add(nums[i]);
+                }
+                else
+                {
+                    indicesImpares.Add(nums[i]);
+                }
+            }
+
+            indicePares.Sort();
+            indicesImpares.Sort();
+            indicesImpares.Reverse();
+
+            List<int> result = new List<int>();
+
+            int indicePar = 0;
+            int indiceImpar = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    result.Add(indicePares[indicePar]);
+                    indicePar++;
+                }
+                else
+                {
+                    result.Add(indicesImpares[indiceImpar]);
+                    indiceImpar++;
+                }
+            }
+
+            return result.ToArray();    
+        }
+
+    }
+}
